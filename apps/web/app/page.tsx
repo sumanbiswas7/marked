6e16d5b3336263f581c2ebff93fbfb4d5b6884b9
sumanbiswas7 <ronaldosuman97@gmail.com>
2023-09-08 16:34:18 +0530

    add: all in one quote

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -11,7 +11,17 @@ export default function Page(): JSX.Element {
         <div className={styles.left_container}>
           <div className={styles.txt_container}>
             <p style={{ color: COLORS.textLightSwatch }}>INTRODUCING</p>
-            <p style={{ color: COLORS.textSwatch }}>Your all in one</p>
+            <p style={{ color: COLORS.textSwatch }}>
+              Your{" "}
+              <span className={styles.all_in_one_txt}>
+                <img src="/home/left_quote.svg" className={styles.left_quote} />
+                all in one
+                <img
+                  src="/home/right_quote.svg"
+                  className={styles.right_quote}
+                />
+              </span>
+            </p>
             <p style={{ color: COLORS.textSwatch }}>
               <span style={{ color: COLORS.green }}>Productivity </span>
               Powerhouse
@@ -23,7 +33,8 @@ export default function Page(): JSX.Element {
               <span style={{ color: COLORS.textSwatch }}> Task </span>Tracking,
               <span style={{ color: COLORS.textSwatch }}> Note </span> Taking
               and <span style={{ color: COLORS.textSwatch }}> Expense </span>
-              Tracking
+              Tracking - All with Handy
+              <span style={{ color: COLORS.textSwatch }}> Reminders</span>.
             </p>
             <div style={{ marginTop: "1rem" }} className={styles.btn_free_cont}>
               <Button title="Get Started" />
