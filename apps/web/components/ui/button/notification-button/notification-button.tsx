906c59d95f89e9c803f87cad06c18fadfaf0c895
sumanbiswas7 <ronaldosuman97@gmail.com>
2023-09-11 15:56:35 +0530

    update: small changes in font header

--- apps/web/components/ui/button/notification-button/notification-button.tsx
+++ apps/web/components/ui/button/notification-button/notification-button.tsx
@@ -1,26 +1,30 @@
 "use client";
 
+import Link from "next/link";
 import { COLORS } from "../../../../theme/colors";
 import styles from "./notification-button.module.scss";
 import { motion } from "framer-motion";
 
 export function NotificationButton({ count }: NotificationProps): JSX.Element {
    return (
-      <motion.div
-         className={styles.notification_container}
-         style={{ backgroundColor: COLORS.textLight2Swatch }}
-      >
-         <img src="/dashboard_header/bell.svg" />
-
-         <motion.span
-            animate={{ scale: [0, 3, 1], y: [3, 0] }}
-            transition={{ duration: 0.5 }}
-            className={styles.count_container}
-            style={{ backgroundColor: count ? COLORS.red : COLORS.green }}
+      <Link href={"/notifications"}>
+         <motion.div
+            whileHover={{ scale: 0.85 }}
+            className={styles.notification_container}
+            style={{ backgroundColor: COLORS.textLight2Swatch }}
          >
-            {count ? count : <img src="/dashboard_header/tick.svg" />}
-         </motion.span>
-      </motion.div>
+            <img src="/dashboard_header/bell.svg" />
+
+            <motion.span
+               animate={{ scale: [0, 3, 1], y: [3, 0] }}
+               transition={{ duration: 0.5 }}
+               className={styles.count_container}
+               style={{ backgroundColor: count ? COLORS.red : COLORS.green }}
+            >
+               {count ? count : <img src="/dashboard_header/tick.svg" />}
+            </motion.span>
+         </motion.div>
+      </Link>
    );
 }
 
