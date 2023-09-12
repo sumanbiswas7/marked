645d423c6bc595be6a410ea9100b122f6f3ea8fa
sumanbiswas7 <ronaldosuman97@gmail.com>
2023-09-12 22:55:32 +0530

    improve: add tests for slice-text

--- apps/web/utils/__test__/slice-text.test.ts
+++ apps/web/utils/__test__/slice-text.test.ts
@@ -2,49 +2,22 @@ import { sliceText } from "../slice-text";
 import { describe, it, expect } from "vitest";
 
 describe("sliceText function", () => {
-   it("should return null for null input", () => {
+   it("should return null for null or undefined input", () => {
       expect(sliceText(null, 40, true)).toBeNull();
-   });
-
-   it("should return null for undefined input", () => {
       expect(sliceText(undefined, 40, true)).toBeNull();
    });
 
-   it("should return the same string if it's shorter than maxLen", () => {
-      expect(sliceText("Short text", 40, true)).toBe("Short text");
+   it("should add ... for a input length 20 and keep the return length 20", () => {
+      const LENGHT_28_INPUT = "hello this is a test alright";
+      const res = sliceText(LENGHT_28_INPUT, 20, true);
+      expect(res?.length).toBe(20);
+      expect(res).toBe("hello this is a t...");
    });
 
-   it("should add '...' to the end of a long string when threeDots is true", () => {
-      expect(
-         sliceText("This is a very long text that needs truncation", 40, true)
-      ).toBe("This is a very long text that needs...");
-   });
-
-   it("should not add '...' to the end of a long string when threeDots is false", () => {
-      expect(
-         sliceText("This is a very long text that needs truncation", 40, false)
-      ).toBe("This is a very long text that needs tr");
-   });
-
-   it("should not add '...' if maxLen is less than 3", () => {
-      expect(sliceText("Short text", 2, true)).toBe("...");
-   });
-
-   it("should handle a string with no spaces by adding '...' to the end", () => {
-      expect(sliceText("Nospacesinthisstring", 15, true)).toBe(
-         "Nospacesinthis..."
-      );
-   });
-
-   it("should handle a string with spaces by adding '...' after the last space", () => {
-      expect(sliceText("This is a long sentence with spaces", 20, true)).toBe(
-         "This is a long sentence..."
-      );
-   });
-
-   it("should pad with spaces if no space is found before maxLen - 3", () => {
-      expect(sliceText("ThisIsAVeryLongWord", 20, true)).toBe(
-         "ThisIsAVeryLongWord..."
-      );
+   it("should return the sliced string and slice upto maxLength", () => {
+      const LENGHT_28_INPUT = "hello this is a test alright";
+      const res = sliceText(LENGHT_28_INPUT, 20, false);
+      expect(res?.length).toBe(20);
+      expect(res).toBe("hello this is a test");
    });
 });
--- apps/web/utils/slice-text.ts
+++ apps/web/utils/slice-text.ts
@@ -3,37 +3,9 @@ export function sliceText(
    maxLen: number,
    threeDots?: boolean
 ): string | null {
+   // BUG: If there's a space at the end it's getting counted with maxLen
    if (!str) return null;
-
    if (str.length <= maxLen) return str;
-
-   if (threeDots && maxLen >= 3) {
-      // Calculate the space available for the text and "..."
-      const availableSpace = maxLen - 3;
-
-      // Check if there's enough space to add "..."
-      if (availableSpace <= 0) {
-         return "...".slice(0, maxLen);
-      }
-
-      // Find the last space before availableSpace
-      const lastSpaceIndex = str.lastIndexOf(" ", availableSpace);
-
-      if (lastSpaceIndex !== -1) {
-         // Add "..." after the last space
-         return (
-            str.slice(0, lastSpaceIndex) +
-            "...".slice(0, maxLen - lastSpaceIndex)
-         );
-      } else {
-         // If no space is found, pad with spaces and add "..."
-         return (
-            str.slice(0, availableSpace) +
-            "...".slice(0, maxLen - availableSpace)
-         );
-      }
-   }
-
-   // If not using "..." or maxLen is too small for "..." to fit, just slice up to maxLen
-   return str.slice(0, maxLen);
+   if (threeDots) return str.slice(0, maxLen + -3) + "...";
+   else return str.slice(0, maxLen);
 }
