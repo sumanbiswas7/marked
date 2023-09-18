788662b31a6fd2b0619f633370960057600c0f72
sumanbiswas7 <ronaldosuman97@gmail.com>
2023-09-18 16:06:40 +0530

    add: some other dev endpoints

---
+++ apps/api/src/controllers/user/all.controller.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response, NextFunction } from "express";
+import { HTTP_STATUS, isValidEmail } from "@marked/utils";
+import { HttpResponse } from "../../models/response";
+import { handleError } from "../../utils/error-handler";
+
+export async function allController(
+   req: Request,
+   res: Response,
+   next: NextFunction
+) {
+   const prisma = new PrismaClient();
+   const success = new HttpResponse({});
+
+   try {
+      const allUsers = await prisma.user.findMany();
+      success.status = HTTP_STATUS.OK;
+      success.message = `All users`;
+      const userArr = Object.values(allUsers);
+      success.data = userArr;
+      res.status(success.status).json(success);
+   } catch (error) {
+      handleError(error)(req, res, next);
+   }
+}
+
+// REVIEW: Delete for dev only
+export async function deleteAllExceptController(
+   req: Request,
+   res: Response,
+   next: NextFunction
+) {
+   const prisma = new PrismaClient();
+   try {
+      const resp = await prisma.user.deleteMany();
+      res.json(resp);
+   } catch (error) {
+      handleError(error)(req, res, next);
+   }
+}
--- apps/api/src/routes/userRoutes.ts
+++ apps/api/src/routes/userRoutes.ts
@@ -4,11 +4,17 @@ import { loginController } from "../controllers/user/login.controller";
 import { bioController } from "../controllers/user/bio.controller";
 import { mockUserController } from "../controllers/user/mock.controller";
 import { signupController } from "../controllers/user/signup.controller";
+import {
+   allController,
+   deleteAllExceptController,
+} from "../controllers/user/all.controller";
 
 const route = Router();
 
 // Routes
 route.get("/mock", mockUserController);
+route.get("/all", allController);
+route.delete("/delete_all", deleteAllExceptController);
 route.post("/login", loginController);
 route.post("/signup", signupController);
 route.get("/bio/:email", bioController);
