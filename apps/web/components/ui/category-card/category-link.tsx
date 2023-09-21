7c69fdc32161696bfa20ff33f10e6d68acccc931
sumanbiswas7 <ronaldosuman97@gmail.com>
2023-09-21 20:51:21 +0530

    fix: link layout

--- apps/web/components/ui/category-card/category-link.tsx
+++ apps/web/components/ui/category-card/category-link.tsx
@@ -6,6 +6,8 @@ import { useTheme } from "../../../hooks/use-theme";
 import { IconTrash } from "@tabler/icons-react";
 import { motion } from "framer-motion";
 import { getFormattedDate } from "../../../utils/format-date";
+import { modals } from "@mantine/modals";
+import { Text } from "@mantine/core";
 
 export function CategoryLink({ link }: Props) {
    const { theme } = useTheme();
@@ -15,17 +17,13 @@ export function CategoryLink({ link }: Props) {
    }
 
    return (
-      <div style={{ position: "relative" }}>
-         <motion.span whileHover={{ scale: 1.2, rotate: 25 }} whileTap={{ scale: 0.8 }}>
-            <IconTrash color={theme.red} size={20} className={styles.icon} onClick={handleDeleteClick} />
-         </motion.span>
-
-         <a href={link.link} target="_blank" style={{ textDecoration: "none" }}>
-            <motion.div
-               whileHover={{ translateX: 10 }}
-               style={{ backgroundColor: theme.card.shade2 }}
-               className={styles.container}
-            >
+      <motion.div
+         style={{ position: "relative", backgroundColor: theme.card.shade2 }}
+         className={styles.main_cont}
+         whileHover={{ translateX: 10 }}
+      >
+         <a href={link.link} target="_blank" style={{ textDecoration: "none" }} className={styles.link_cont}>
+            <motion.div className={styles.container}>
                <div className={styles.top_box} style={{ marginBottom: 5, color: theme.text.shade1 }}>
                   <p>{link.title || "No Title"}</p>
                </div>
@@ -44,8 +42,29 @@ export function CategoryLink({ link }: Props) {
                </div>
             </motion.div>
          </a>
-      </div>
+
+         <motion.span whileHover={{ scale: 1.5 }} whileTap={{ scale: 0.8 }} className={styles.icon_cont}>
+            <IconTrash color={theme.red} size={20} className={styles.icon} onClick={openLinkDeleteModal} />
+         </motion.span>
+      </motion.div>
    );
+
+   /**
+    * ------------------
+    *       Modal
+    * ------------------
+    */
+
+   function openLinkDeleteModal() {
+      modals.openConfirmModal({
+         title: `Delete Confirmation`,
+         centered: true,
+         children: <Text size="sm">Please be aware that the link {link.link} will be deleted forever.</Text>,
+         labels: { confirm: "Delete link", cancel: "No don't delete it" },
+         confirmProps: { color: "red" },
+         // onConfirm: () => mutation.mutate(),
+      });
+   }
 }
 
 interface Props {
