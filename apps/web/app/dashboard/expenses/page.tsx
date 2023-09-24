93ee0178d917d898e67d122f6f3ab59bc5a3a7ff
sumanbiswas7 <ronaldosuman97@gmail.com>
2023-09-24 11:56:49 +0530

    update: make the whole dashboard client component

--- apps/web/app/dashboard/expenses/page.tsx
+++ apps/web/app/dashboard/expenses/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import styles from "./expenses.module.scss";
 
 export default function DashboardExpensesPage(): JSX.Element {
--- apps/web/app/dashboard/notes/page.tsx
+++ apps/web/app/dashboard/notes/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import styles from "./notes.module.scss";
 
 export default function DashboardNotesPage(): JSX.Element {
