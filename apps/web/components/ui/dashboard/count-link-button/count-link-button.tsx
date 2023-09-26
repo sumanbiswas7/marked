e8c8b758973646635c77db4590a4b3c2c01fed10
sumanbiswas7 <ronaldosuman97@gmail.com>
2023-09-26 12:53:49 +0530

    add: anims on count link

--- apps/web/components/ui/dashboard/count-link-button/count-link-button.tsx
+++ apps/web/components/ui/dashboard/count-link-button/count-link-button.tsx
@@ -2,19 +2,27 @@
 
 import { useTheme } from "../../../../hooks/use-theme";
 import styles from "./count-link-button.module.scss";
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 export function CountLinkButton({ href, count, description, title }: Props) {
    const { theme } = useTheme();
 
    return (
-      <Link href={href} className={styles.container} style={{ backgroundColor: theme.accent }}>
-         <p style={{ color: theme.text.shade3 }}>{title}</p>
-         <p style={{ color: theme.text.shade2 }}>{description}</p>
+      <Link href={href} style={{ textDecoration: "none" }}>
+         <motion.div
+            whileHover={{ y: 5 }}
+            whileTap={{ scale: 0.8 }}
+            className={styles.container}
+            style={{ backgroundColor: theme.accent }}
+         >
+            <p style={{ color: theme.text.shade3 }}>{title}</p>
+            <p style={{ color: theme.text.shade2 }}>{description}</p>
 
-         <span className={styles.count} style={{ backgroundColor: theme.text.shade2, color: theme.text.shade1 }}>
-            {count}
-         </span>
+            <span className={styles.count} style={{ backgroundColor: theme.text.shade2, color: theme.text.shade1 }}>
+               {count}
+            </span>
+         </motion.div>
       </Link>
    );
 }
