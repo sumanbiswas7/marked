d7fc25bd5dfa1512451b5ce87245a14e217eab80
sumanbiswas7 <ronaldosuman97@gmail.com>
2023-09-26 11:33:37 +0530

    add: dash top row count link btns

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -5,13 +5,30 @@ import styles from "./page.module.scss";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../../hooks/use-auth";
 import { Clock } from "../../components/ui/clock/clock";
+import { DashboardCountLinkButton } from "../../components/ui/dashboard-count-link-button/dashboard-count-link-button";
 
 export default function DashboardPage(): JSX.Element {
    return (
-      <div>
-         <h1>Dashboard</h1>
-         <p>Conming Soon...</p>
-         <Clock />
+      <div className={styles.main_container}>
+         <div className={styles.left_container}>
+            <div className={styles.top_row}>
+               <DashboardCountLinkButton
+                  count={12}
+                  title="Notes"
+                  description="View your notes"
+                  href="/dashboard/notes"
+               />
+               <DashboardCountLinkButton
+                  count={12}
+                  title="Saved Links"
+                  description="View saved links"
+                  href="/dashboard/categories"
+               />
+            </div>
+         </div>
+         <div className={styles.right_container}>
+            <Clock />
+         </div>
       </div>
    );
 }
---
+++ apps/web/components/ui/dashboard-count-link-button/dashboard-count-link-button.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useTheme } from "../../../hooks/use-theme";
+import styles from "./dashboard-count-link-button.module.scss";
+import Link from "next/link";
+
+export function DashboardCountLinkButton({ href, count, description, title }: Props) {
+   const { theme } = useTheme();
+
+   return (
+      <Link href={href} className={styles.container} style={{ backgroundColor: theme.accent }}>
+         <p style={{ color: theme.text.shade3 }}>{title}</p>
+         <p style={{ color: theme.text.shade2 }}>{description}</p>
+
+         <span className={styles.count} style={{ backgroundColor: theme.text.shade2, color: theme.text.shade1 }}>
+            {count}
+         </span>
+      </Link>
+   );
+}
+
+interface Props {
+   title: string;
+   description: string;
+   href: string;
+   count: number;
+}
