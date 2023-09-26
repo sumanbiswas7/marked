fe8702b0df5d5cc98d929a799b4e207d97071a0e
sumanbiswas7 <ronaldosuman97@gmail.com>
2023-09-26 22:00:20 +0530

    update: add border and copy to clipboard

--- apps/web/components/ui/dashboard/link-in-bio/link-in-bio.tsx
+++ apps/web/components/ui/dashboard/link-in-bio/link-in-bio.tsx
@@ -5,16 +5,25 @@ import { StatsWrapper } from "../stats-wrapper/stats-wrapper";
 import Link from "next/link";
 import { useTheme } from "../../../../hooks/use-theme";
 import { IconClipboard, IconEdit } from "@tabler/icons-react";
+import { successNotification } from "../../../../utils/show-notifications";
 
 export function LinkInBio() {
    const { theme } = useTheme();
+
+   function onCopyText() {
+      navigator.clipboard.writeText("https://chat.openai.com/");
+      successNotification("Link copied to clipboard");
+   }
 
    return (
       <StatsWrapper
          title="Link in bio"
          description="Unlock Your Online Presence add Your Social Media Links and Share Them in One Click!  "
       >
-         <div className={styles.buttons} style={{ backgroundColor: theme.card.shade2 }}>
+         <div
+            className={styles.buttons}
+            style={{ backgroundColor: theme.card.shade2, borderColor: theme.border.shade1 }}
+         >
             <Link className={styles.btn} href={"/profile"} style={{ color: theme.text.shade1 }}>
                Edit Profile{" "}
                <span>
@@ -22,7 +31,7 @@ export function LinkInBio() {
                </span>
             </Link>
             <div className={styles.divider} />
-            <button className={styles.btn} style={{ color: theme.text.shade1 }}>
+            <button className={styles.btn} style={{ color: theme.text.shade1 }} onClick={onCopyText}>
                Copy Link{" "}
                <span>
                   <IconClipboard color={theme.text.shade1} size={15} />
